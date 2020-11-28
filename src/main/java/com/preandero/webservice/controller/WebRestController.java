e6d4d39222196a0d2478ec42e03abc70f3a1f701
preandero <preandero@gmail.com>
2020-11-29 00:06:45 +0900

    first commit

---
+++ src/main/java/com/preandero/webservice/controller/WebRestController.java
@@ -0,0 +1,14 @@
+package com.preandero.webservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+  @GetMapping("/hello")
+  public String hello(){
+    return "HelloWorld";
+  }
+
+}
